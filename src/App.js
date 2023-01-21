8625a14eb9eff202201a32932d4bb082a6072e08
Aram <arma-arman@mail.ru>
2023-01-21 11:47:08 +0400

    commited all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [temperature, setTemperature] = useState(10);
+  const [color, setColor] = useState("cold");
+
+  const oneMore = temperature + 1;
+  const oneLess = temperature - 1;
+
+  const funcOneMore = () => {
+    if(temperature > 15) {
+      setColor("hot");
+    } 
+    if(temperature > 25) {
+      setColor("warm");
+    } 
+    if(temperature > 50) {
+      setColor("veryHot");
+    } 
+    if(temperature >= 100) {
+      setColor("veryVeryHot");
+    }
+
+    setTemperature(oneMore);
+  }
+
+  const funcOneLess =() => {
+    if(temperature < 15) {
+      setColor("cold");
+    } 
+    if(temperature < 0) {
+      setColor("veryCold");
+    } 
+
+    setTemperature(oneLess);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className={`temperature ${color}`}>
+          <h1>${temperature}°C</h1>
+        </div>
+        <div className="managment">
+          <button onClick={funcOneMore} className="plus">+</button>
+          <button onClick={funcOneLess} className="minus">-</button>
+        </div>
+      </div>
     </div>
   );
 }
